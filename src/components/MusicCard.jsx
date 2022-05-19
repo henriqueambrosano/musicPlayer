9ec6f5d93f97e605bd363bd122c3a7b507686b13
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-05-19 17:24:25 -0300

    requisito 11

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
   constructor(props) {
     super(props);
     this.addAsFavoriteSong = this.addAsFavoriteSong.bind(this);
+    const { isChecked } = this.props;
+
+    this.state = {
+      loading: false,
+      isFavorite: isChecked,
+    };
   }
 
-  state = {
-    loading: false,
-    isFavorite: false,
-  }
-
-  async addAsFavoriteSong() {
+  async addAsFavoriteSong({ target }) {
+    const { checked } = target;
     this.setState((prev) => ({ loading: true, isFavorite: !prev.isFavorite }));
-    await addSong(this.props);
+    if (checked) {
+      await addSong(this.props);
+    } else {
+      await removeSong(this.props);
+    }
     this.setState({ loading: false });
     getFavoriteSongs();
   }
 
   render() {
-    const { previewUrl, trackName, trackId, isChecked } = this.props;
+    const { previewUrl, trackName, trackId } = this.props;
     const { loading, isFavorite } = this.state;
     const loadingElement = <p>Carregando...</p>;
     return (
@@ -35,12 +41,12 @@ class MusicCard extends React.Component {
         </audio>
         {loading ? loadingElement
           : (
-            <label htmlFor="favorite">
+            <label htmlFor={ trackName }>
               <input
-                checked={ isChecked || isFavorite }
+                checked={ isFavorite }
                 name="favoriteSongs"
                 type="checkbox"
-                id="favorite"
+                id={ trackName }
                 data-testid={ `checkbox-music-${trackId}` }
                 onChange={ this.addAsFavoriteSong }
               />
